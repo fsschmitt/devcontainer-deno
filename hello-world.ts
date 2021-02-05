5214679c624169135b3cf3609e954efc21098606
Alexandre Santos <alexandre.santozz@gmail.com>
2021-02-05 12:43:08 +0000

    chore(fmt): run deno-fmt on hello-world.ts

--- hello-world.ts
+++ hello-world.ts
@@ -2,17 +2,17 @@
  * hello-world.js
  */
 function capitalize(word: string): string {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  }
-  
-  function hello(name: string): string {
-    return "Hello " + capitalize(name);
-  }
-  
-  console.log(hello("john"));
-  console.log(hello("Sarah"));
-  console.log(hello("kai"));
-  /**
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+function hello(name: string): string {
+  return "Hello " + capitalize(name);
+}
+
+console.log(hello("john"));
+console.log(hello("Sarah"));
+console.log(hello("kai"));
+/**
    * Output:
    *
    * Hello John
